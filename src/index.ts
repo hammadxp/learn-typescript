309f3a074eea1d8294dcc1f734fc7403b0cb14d0
hammadxp <hammadjaved9860@gmail.com>
2023-07-14 23:50:39 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+// --- Welcome ---
+
+// console.log("Hello World");
+
+// --- Debug ---
+
+// let age: number = 23;
+
+// if (age < 50) {
+//   age += 10;
+// }
+
+// --- any Type ---
+
+// let sales: number = 123_456_789;
+// let course: string = "Typescript";
+// let isFree: boolean = true;
+
+// let what;
+// what = 10;
+// what = "a";
+
+// function render(document) {
+//   console.log(document);
+// }
+
+// --- Array Type ---
+
+// let numbers: number[] = [1, 2, "3"];
+// let numbers = [1, 2, 3];
+// let numbers = [];
+
+// --- Tuple Type ---
+
+// let user: [number, string] = [123, "Hammad"];
+// user.push(52);
